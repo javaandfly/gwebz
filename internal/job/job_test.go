352ae1fb463ca889b30a326086c248e22c58be30
xiaodong <373232355@qq.com>
2023-12-12 14:42:53 +0800

    Add goroutine processing to tasks

---
+++ internal/job/job_test.go
@@ -0,0 +1,27 @@
+package job
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestJobEngine(t *testing.T) {
+	start := time.Now()
+	job := NewJobEngine(context.Background(), 10000000)
+	for i := 0; i < 10000; i++ {
+		job.AddTask(add)
+	}
+	job.JobTimingHandle()
+
+	elapsed := time.Since(start)
+	fmt.Println("该函数执行完成耗时：", elapsed)
+
+}
+
+func add(context *JobContext) {
+	for i := 0; i < 10; i++ {
+
+	}
+}
